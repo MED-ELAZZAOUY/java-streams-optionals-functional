a811baf3342eb349fc82fe8bd5e2bc571c019c2f
Mohamed EL Azzaouy <medelazzaouy99@gmail.com>
2023-09-14 13:08:29 +0100

    adding models/Utils

--- src/main/java/net/elazzaouy/Main.java
+++ src/main/java/net/elazzaouy/Main.java
@@ -1,7 +1,30 @@
 package net.elazzaouy;
 
+import net.elazzaouy.model.Gender;
+import net.elazzaouy.model.Person;
+import net.elazzaouy.utils.DataTransformationUtils;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Person> people = getPeole();
+        people.stream()
+                .map(DataTransformationUtils::toJson)
+                .forEach(System.out::println);
+
+    }
+
+    private static List<Person> getPeole(){
+        return List.of(
+                new Person("MED ", 24, Gender.MALE),
+                new Person("Ayoub", 20, Gender.MALE),
+                new Person("Khadija", 18, Gender.FEMALE),
+                new Person("Hind", 21, Gender.FEMALE),
+                new Person("Ali", 65, Gender.MALE),
+                new Person("Abdo", 100, Gender.MALE),
+                new Person("Ilyass", 7, Gender.MALE ),
+                new Person("Fatima", 85, Gender.FEMALE)
+        );
     }
 }
---
+++ src/main/java/net/elazzaouy/model/Gender.java
@@ -0,0 +1,5 @@
+package net.elazzaouy.model;
+
+public enum Gender {
+    MALE, FEMALE
+}
